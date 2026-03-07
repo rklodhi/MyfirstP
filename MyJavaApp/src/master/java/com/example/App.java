7346e783802b46fc314b97994b95106d89b98cf6
ci-cd <ci-cd@gmail.com>
2026-03-07 12:03:25 +0530

    Keep container alive to avoid CrashLoopBackOff

--- MyJavaApp/src/master/java/com/example/App.java
+++ MyJavaApp/src/master/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from Jenkins Maven Build!");
+
+        while (true) {
+            Thread.sleep(10000);
+        }
     }
 }
